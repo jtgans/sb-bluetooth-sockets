4740afdf7e0566c46c3e5ce967349e4125c9621d
June Tate-Gans <june@theonelab.com>
2016-04-26 19:39:05 -0400

    rfcomm: Prefix socket method specializations
    Apparently the package definition problem extends to defmethods as well,
    necessitating the prefix of the sb-bsd-sockets package name to the names
    of the defmethod specializations.
    
    Annoying. Perhaps I need to learn more about CLOS?

--- rfcomm.lisp
+++ rfcomm.lisp
@@ -4,17 +4,17 @@
   ((sb-bsd-sockets::family :initform af-bluetooth))
   (:documentation "Class representing RFCOMM bluetooth sockets (AF_BLUETOOTH)."))
 
-(defmethod socket-namestring ((socket rfcomm-socket))
+(defmethod sb-bsd-sockets::socket-namestring ((socket rfcomm-socket))
   (ignore-errors (socket-name socket)))
 
-(defmethod socket-peerstring ((socket rfcomm-socket))
+(defmethod sb-bsd-sockets::socket-peerstring ((socket rfcomm-socket))
   (ignore-errors (socket-peername socket)))
 
 (defun string-to-bdaddr (bdaddrstr)
   (multiple-value-bind (retval bdaddr) (string-to-bdaddr-int bdaddrstr)
     bdaddr))
 
-(defmethod make-sockaddr-for ((socket rfcomm-socket) &optional sockaddr &rest address)
+(defmethod sb-bsd-sockets::make-sockaddr-for ((socket rfcomm-socket) &optional sockaddr &rest address)
   (let ((address (first address))
         (channel (second address))
         (sockaddr (or sockaddr (allocate-sockaddr-rc))))
@@ -24,13 +24,13 @@
       (setf (sockaddr-rc-bdaddr sockaddr) (string-to-bdaddr address)))
     sockaddr))
 
-(defmethod free-sockaddr-for ((socket rfcomm-socket) sockaddr)
+(defmethod sb-bsd-sockets::free-sockaddr-for ((socket rfcomm-socket) sockaddr)
   (free-sockaddr-rc sockaddr))
 
-(defmethod size-of-sockaddr ((socket rfcomm-socket))
+(defmethod sb-bsd-sockets::size-of-sockaddr ((socket rfcomm-socket))
   size-of-sockaddr-rc)
 
-(defmethod bits-of-sockaddr ((socket rfcomm-socket) sockaddr)
+(defmethod sb-bsd-sockets::bits-of-sockaddr ((socket rfcomm-socket) sockaddr)
   "Return the address of the RFCOMM socket."
   (let ((name (sockaddr-rc-bdaddr sockaddr)))
     (unless (zerop (length name))
